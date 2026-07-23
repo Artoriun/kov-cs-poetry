0c3168b71b8fa8c331274cc0c7b18615d76ec107
Peter de Keijzer <pjcr.dekeijzer@gmail.com>
2026-07-24 03:58:47 +0900

    merge Óda a testemhez into one poem with 2 custom slides
    The two book pages of Óda a testemhez (same image, continuous text) are now
    a single poem using customSlides. Kaposszentbenedek stays as two separate
    poems (distinct text + different images). Renumbered to 34 poems.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -182,80 +182,77 @@ export const POEMS: Poem[] = [
       'https://res.cloudinary.com/dgk299isx/image/upload/v1784828938/poems/oda-a-testemhez.jpg',
     overlay:
       'Mint könnyű nád hajoltál egykor zsenge\nTavánál életemnek, s mint a penge\nHasítottad a levegőt, ha vitt\nKét lábad: e két testvérparipa\nSzökellve vágtatva: hol ott, hol itt…\n\nŰrhajó nem volt még, de a szemed\nMégis a tejútrendszeren túl látta\nEgy álmodott világ szépségeit.\n\nEmlékszel, mikor madarat temettél,\nA földön guggolva, ástad a sírt?\nA halál mégse izgatott. Te lettél az alfa és omega, akit\nKegyes sorsod múló habokra írt.\n\nMint könnyű nád, óh, úgy hajoltál, testem\nA lányhoz, aki szőke volt és sudár,\nS úgy ölelted át loncként, mint a nyár\nBúzavirágos mezőt bíbor esten.\n\nTe voltál a gyorsaság és a szépség,\nMindent megértés: iramló határ,\nGondolatod gondolatot keresztez…\nS védni magad megálltál, mint a vár.',
+    customSlides: [
+      'Mint könnyű nád hajoltál egykor zsenge\nTavánál életemnek, s mint a penge\nHasítottad a levegőt, ha vitt\nKét lábad: e két testvérparipa\nSzökellve vágtatva: hol ott, hol itt…\n\nŰrhajó nem volt még, de a szemed\nMégis a tejútrendszeren túl látta\nEgy álmodott világ szépségeit.\n\nEmlékszel, mikor madarat temettél,\nA földön guggolva, ástad a sírt?\nA halál mégse izgatott. Te lettél az alfa és omega, akit\nKegyes sorsod múló habokra írt.\n\nMint könnyű nád, óh, úgy hajoltál, testem\nA lányhoz, aki szőke volt és sudár,\nS úgy ölelted át loncként, mint a nyár\nBúzavirágos mezőt bíbor esten.\n\nTe voltál a gyorsaság és a szépség,\nMindent megértés: iramló határ,\nGondolatod gondolatot keresztez…\nS védni magad megálltál, mint a vár.',
+      'Csak tegnap volt, hogy hajnali szelekkel\nHalhatatlanság tündérszárnya verte\nKörülötted a nap aranyporát,\nS mért perceid már fáradt cimborák?\n\nKi győz majd benned, hogyha egyre vénül\nAz idő körülötted és dagad,\nFejeden barna hajad is csak fehérül\nÉs a halálnak megadod magad?\n\nS mi eszme volt, élet, bizodalom,\nBölcsesség, ész, gyönyör, vad fájdalom,\nKihajítódik egy kis ablakon,\nAmelynek szárnya bamba űrbe zárva,\nS két gyermekedről azt mondják, hogy árva…\n\nTested pedig, nem mint meteor\nGipszholdra hullva méltó mód enyész el,\nDe úgy rohad. Érted, látod? Tudod?\nMint a vegytan rohad el a vegyésszel.\n\nÍnycsiklandó étellel és itallal,\nLásd a halált hizlaltad hát magadban.\nÓh, testem, árnyékom: dal a dalban:\nÁtkozlak, hogy vagy s hogy benned vagyok:\nRajongva, őrülten és aztán halkan… halkan…..',
+    ],
+    customSlidesEnabled: true,
   },
   {
     id: 'poem-24',
-    title: 'Óda a testemhez',
-    image:
-      'https://res.cloudinary.com/dgk299isx/image/upload/v1784828938/poems/oda-a-testemhez.jpg',
-    overlay:
-      'Csak tegnap volt, hogy hajnali szelekkel\nHalhatatlanság tündérszárnya verte\nKörülötted a nap aranyporát,\nS mért perceid már fáradt cimborák?\n\nKi győz majd benned, hogyha egyre vénül\nAz idő körülötted és dagad,\nFejeden barna hajad is csak fehérül\nÉs a halálnak megadod magad?\n\nS mi eszme volt, élet, bizodalom,\nBölcsesség, ész, gyönyör, vad fájdalom,\nKihajítódik egy kis ablakon,\nAmelynek szárnya bamba űrbe zárva,\nS két gyermekedről azt mondják, hogy árva…\n\nTested pedig, nem mint meteor\nGipszholdra hullva méltó mód enyész el,\nDe úgy rohad. Érted, látod? Tudod?\nMint a vegytan rohad el a vegyésszel.\n\nÍnycsiklandó étellel és itallal,\nLásd a halált hizlaltad hát magadban.\nÓh, testem, árnyékom: dal a dalban:\nÁtkozlak, hogy vagy s hogy benned vagyok:\nRajongva, őrülten és aztán halkan… halkan…..',
-  },
-  {
-    id: 'poem-25',
     title: 'Eros',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828908/poems/eros.jpg',
     overlay:
       'A télben is lobog örök, ősi lánggal a szerelem,\nVilágok elmúlhatnak, országok eltűnhetnek, de a nagy Erosz\nKüllőző Vággyal mindennap felhág az élet egén, talányos ereidben vad sejtelemmel ott terem,\nS végzetként űz, hajt új föld és új istenek felé\nEz a legerősebb elem.\nGondolhatsz bármit, árnyéknak nézve élted,\nPiramisok tövén, dómok homályos mélyén,\nÁtokkal és halállal el nem éred.\nÖrök szépséggel, fénnyel jár ő a küzdő\nKatona oldalán, s rothadt tetem\nBűzét egyetlen ujja illatával\nTavasszá oszlathatja széjjel.\nMindent elönthet vak, halálos Éjjel,\nCsak Erosz halhatatlan, testetlen ő,\nSzellem? Vágy? Akarat? vagy tán Erő?\nNevezd el bárminek, eszed szemével úgyse\nLáthatod meg, de forrsz, mint lomha üst,\nHa jön, s lefog őszi erdők alatt,\nS szoknyája úgy lebeg a szélben, mint a füst…',
   },
   {
-    id: 'poem-26',
+    id: 'poem-25',
     title: 'Cím nélkül',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828906/poems/cim-nelkul.jpg',
     overlay:
       'Nem tudok írni magának\nhelyettem elmondják a fák\nNem szeretlek csak úgy cicázok\nS este nem gondolok rád.',
   },
   {
-    id: 'poem-27',
+    id: 'poem-26',
     title: 'Nofre-tete',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828925/poems/nofretete.jpg',
     overlay:
       'Királynőm, Nofre-tetém\nHiszed-e, hogy lényed piramisát\nbe nem szitálja rút időhomok?\ns örök nyomok\nbeszélik, hogy Te is a földön jártál?\nHiszed-e, hogy nem hervad el a mosolyod?\nhogy múlhatatlanok a szemeid,\ns hogy lelked ibiszmadarának\na szárnyai a végtelenbe vágnak\ntúl a Napon?\n\nAz ember, nézd, halandó lárva,\ns mindenkiben egy pillangó remeg\ns életre vár\nde rövid bábálma alatt vedleni nem tud,\nszomorú létbe zárva,\ncsak ha Ozirisz országa fénylik….\n\nS ha csókolsz, mindig fénylik Nofre-tetém,\naz ő országa közeleg.\nMaga Ozirisz jön elébünk,\ns lecsukja lassan a szemem, szemed,\naztán visz minket túl a Níluson,\ns amíg megyünk,\nmégegyszer hallgatag\na nagy Életszfinksz ránkmered….',
   },
   {
-    id: 'poem-28',
+    id: 'poem-27',
     title: 'Vízió',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828935/poems/vizio.jpg',
     overlay:
       'Óra vagyok a tested dómján\nS a tested dómja mállik egyre\nMaholnap ott fekszem a romján.\n\nÓra vagyok a tested dómján\nS éjfélkor bús sohse lesz asszony\nHalk lépése közelít erre\nMegállítni a mutatókat.\n\nJön, jön. Kezét már nyújtja, nyújtja\nSzétzúzni az arcomat készül,\nhanem egyszer csak meginog,\nA falépcsőkön visszaszédül\nS valami lök a mutatókon.',
   },
   {
-    id: 'poem-29',
+    id: 'poem-28',
     title: 'Temps perdu',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828933/poems/temps-perdu.jpg',
     overlay:
       'Ha festő volnék, megfesteném a szádat,\nDe előbb kékre-zöldre festeném a vásznat.\nAzután a hajadat szőkének,\nAz arcodat rózsásnak\nA ruhádat pirospettyesnek\nA karodat pelyhespuhának\nMégegyszer megnézném\nÉs az egészet összetépném.\nMert egy kép, bármi gyönyörű\nAz életből csak ellopott bánat.\nAz isten is festő volt.\nGyönyörűen megfestett téged.\nSzép fejet adott néked,Szép alakot, csodásat,\nJó hű szívet is, tüzességet,\nEgészséget, pirospozsgásat,\nÉs mégse jó a kép,\nMért szállnak a felhők\nÉs mért hervadnak el a jegenyék,\nHa nézlek,\nAz Isten is tudja ezt\nÉs összetép\nMeglátod, egyszer összetép.',
   },
   {
-    id: 'poem-30',
+    id: 'poem-29',
     title: 'Így csak',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828937/poems/igy-csak.jpg',
     overlay:
       'Ködgyermekek\n\nFiaim, ti csillagmosolyok,\nHolt fények, felhők, álmodások\nMenjetek vissza föl az égbe\nAhol csillog csillaganyátok.\n\nEz a föld nem nektek való\nItt testek vannak, csókok és lázadások,\nhejehuja vér, cifrázott szavak\nAzért halt meg csillaganyátok.\n\nDecember volt, jött, álmodott a tél,\nAnyátok úgy csókolt, mint a tavasz\nÉs elment messze, hű, örök hazába\nS a távolság megölte, a ravasz.\n\nVisszajött még, de bús idegen\nSzemmel nézett rám, mint ki sose volt\nApátokba szerelmes, hidegen\nKihűlten állt meg, mint a téli hold.\n\nAz ujja egy csillagra mutatott,\nSzerelme odaszállt el, mellyel\nEngem szeretett, álmodó apátok,\nÉs elfutott kidülledt mellel.\n\nTüzét és vérét másnak viszi el,\nNekem a csillag marad és az átok,\nFiaim, csillagmosolyok,\nHolt fények, felhők, álmodások,\nMenjetek vissza föl az égbe,\nAhol csillog csillaganyátok.',
   },
   {
-    id: 'poem-31',
+    id: 'poem-30',
     title: 'Könny',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828916/poems/konny-1.jpg',
     overlay:
       'Az ember e szomorú virág\nÉppúgy termi a könnyet\nAkár a rózsalevél,\nVagy fű a réten a harmatot.\nha nincs, ki felszárítsa\nBegyűrűznek a könnyek\nLátatlan levelekbe\nÉs ott tovább gyöngyöznek.\n\nHa nincs, ki felszárítsa,\nKicsurrannak a földbe\nMert már olyan sokan vannak\nS ilyenkor egy marék\nFöldet veszünk kezünkbe,\nS mondjuk: Föld, édesanyánk\nMilyen messze az ég.\nHogy nem szakadt reánk.',
   },
   {
-    id: 'poem-32',
+    id: 'poem-31',
     title: 'Macbeth',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828922/poems/macbeth.jpg',
     overlay:
       'Machbeth az álmot gyilkolja\n\nA sorsom Lady Machbeth\nRagyogó szemű asszony,\nDe rút és bujtó, s most nem alszom:\nÁlmokat gyilkoltat velem,\nS ha nem teszem meg, megvet.',
   },
   {
-    id: 'poem-33',
+    id: 'poem-32',
     title: 'Shakespeare',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828929/poems/shakespeare.jpg',
     overlay:
       'Hogy lelkemben lépked a dán királyfi,\nS Machbeth boszorkányi itt huhognak,\nNeked kellett előbb a földre szállni\nTe óriási hattyúja az Avonnak.\n\nHa Lear király dühöng, Prospero varázst mormol,\nVíg harca tombol szép Titániának,\nTe üzensz egy elmúlt, fenséges korból,\nS tündéreid a homlokomra szállnak.\n\nKi volt nagyobb: Shakespeare vagy az élet?\nHa tenger ez, kimerted a felét már,\nDe nagy korom újból az égig éled\nS tekintetem hullámiból feléd száll.\n\nÍgy állok én, Idő és Törvény felettem:\nVésőm szikrázik a kemény anyagban,\nSzférák zenéje hallik önfeledten,\nS átlépem a vizet, mely zúg alattam.',
   },
   {
-    id: 'poem-34',
+    id: 'poem-33',
     title: 'Shakespeare halála',
     image:
       'https://res.cloudinary.com/dgk299isx/image/upload/v1784828928/poems/shakespeare-halala.jpg',
@@ -263,7 +260,7 @@ export const POEMS: Poem[] = [
       'Valami nagyszerűt, csodást akartam,\nMert az ifjúság álmokkal teli,\nÉs írtam, írtam szavakba takartan\nMert az álom jogát követeli.\n\nVersenyre kelni akartam Shakespearrel\nStratford of Avon - Új Szentbenedek\nDe megjártam a dicsőséggel, hírrel\nS éveim száma lepereg.\n\nVagyok most is szánalmas ismeretlen\nÜt rajtam minden aljas törtető,\nNincsen nevem, s régóta nem nevettem,\nÉletem felett nincsen még tető.\n\nS akiben mintaképem véltem,\nVégezve dolgát, az is elhagyott,\nÖtvenkét esztendőt - íme - megéltem,\nÉs mégis, mégis sehol sem vagyok.',
   },
   {
-    id: 'poem-35',
+    id: 'poem-34',
     title: 'Vigasztalás',
     image: 'https://res.cloudinary.com/dgk299isx/image/upload/v1784828934/poems/vigasztalas.jpg',
     overlay:
